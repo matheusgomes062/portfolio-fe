ec36570e7f4ad2e1da2a29264142766086d71213
matheusgomes062 <matheusgomes062@gmail.com>
2022-12-22 18:35:00 -0300

    feat: add experience card and about page

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -8,9 +8,13 @@ import cn from 'classnames';
 
 interface ExperienceCardProps {
   /**
-   * Title ExperienceCard
+   * Role when working on the company
    */
-  title: string;
+  role: string;
+  /**
+   * Company name
+   */
+  company: string;
   /**
    * Company link source
    */
@@ -24,102 +28,49 @@ interface ExperienceCardProps {
    */
   images: Array<{alt: string, image: string}>;
   /**
-   * Date range for your experience
+   * startDate for your experience
    */
-  date: string;
-}
-
-function BannerProject({source}: {source: string | undefined}) {
-  if (source) {
-    return <Image
-          alt="Project image"
-          src={source}
-          width={600}
-          height={150}
-          style={{
-            minWidth: '100%',
-            height: '150px',
-            objectFit: "cover"
-          }}
-          className="rounded-t-lg"
-        />;
-  }
-  return <div className="w-full h-[150px] rounded-t-lg bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"/>;
-}
-
-function TagTheme(color: string) {
-  const theme = [
-    {color: 'green', class:'bg-green-200 text-green-700'},
-    {color: 'blue', class:'bg-blue-200 text-blue-700'},
-    {color: 'yellow', class:'bg-yellow-200 text-yellow-700'},
-    {color: 'red', class:'bg-red-200 text-red-700'},
-    {color: 'gray', class:'bg-gray-200 text-gray-700'},
-    {color: 'pink', class:'bg-pink-200 text-pink-700'},
-    {color: 'purple', class:'bg-purple-200 text-purple-700'},
-    {color: 'orange', class:'bg-orange-200 text-orange-700'}
-  ].filter(item =>
-    item.color === color
-  )
-  return theme[0].class.toString()
+  startDate: string;
+  /**
+   * startDate for your experience
+   */
+  endDate: string;
 }
 
 const ExperienceCard: FunctionComponent<ExperienceCardProps> = ({
-  title,
+  role,
+  company,
   source,
-  externalLink,
-  githubLink,
-  summary,
-  ...props
+  description,
+  images,
+  startDate,
+  endDate
 }) => {
   return (
-    <div className="relative group transform hover:scale-[1.01] transition-all">
-      <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-    <div className="sm:w-full h-[27rem] md:h-[28rem] bg-zinc-100 rounded-lg drop-shadow-xl dark:bg-gray-800 dark:border-gray-700 ring-1 ring-gray-900/5">
-        <a href={externalLink || githubLink}>
-          <BannerProject source={source}/>
-        </a>
-        <div className="px-5 pb-5">
-            <a href={externalLink || githubLink}>
-                <h5 className="text-xl font-semibold tracking-tight pt-5 text-gray-900 dark:text-white">
-                  {title}
-                </h5>
-            </a>
-            <div className='h-20 md:h-20 mb-3 border-b border-gray-300 dark:border-gray-700 text-gray-900'>
-              {
-                props.tags?.map((object: {label: string, color: string}, i: number) =>
-                <div
-                className={cn(`cursor-pointer text-[10px] inline-flex items-center font-bold leading-sm uppercase px-2 rounded-full mr-2 transform hover:scale-[1.05] transition-all ${TagTheme(object.color)}`
-                )}
-                key={i}
-                >
-                    {object.label}
-                  </div>
-                )
-              }
-            </div>
-            <div className='text-start relative mt-1 text-sm h-[4rem] md:h-[5rem]'>
-              <p className='absolute bottom-0'>
-                {summary}
-              </p>
-            </div>
-            <div className='flex flex-row items-center fixed bottom-5 gap-5'>
-              { externalLink && <Button
-                  aria="Visit External Link"
-                  link={externalLink}
-                  >Visit
-                    <HiExternalLink/>
-                </Button>
-              }
-              
-              { githubLink && <Button isIcon="false"
-                  link={githubLink}>
-                <FaGithub/>
-              </Button>
-              }
-            </div>
-        </div>
-    </div>
-  </div>
+    <section className='bg-gray-200 dark:bg-gray-800 p-5 rounded-lg'>
+      <div className='flex flex-row justify-between gap-3'>
+        {
+          images.map((object: {alt: string, image: string}, i: number) =>
+            <Image
+              key={i}
+              className="rounded-lg md:self-center even:w-[50%]"
+              src={`/${object.image}`}
+              alt={object.alt}
+              width={150}
+              height={150}
+              style={{
+                minWidth: '22%',
+                height: '100px',
+                objectFit: "cover"
+              }}
+              />
+          )
+        }
+      </div>
+      <h3 className='mb-0'>{role} @ <a href={source} className='uppercase'>{company}</a></h3>
+      <p className='text-xs text-gray-400 dark:text-gray-400 uppercase'>{startDate} - {endDate}</p>
+        <p>{description}</p>
+  </section>
   )
 }
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 import Container from '@/components/Container';
 import avatar from 'public/profile-pic.png';
+import ExperienceCard from '@/components/ExperienceCard';
 
 export default function About() {
   return (
@@ -32,99 +33,55 @@ export default function About() {
           </ul>
           <section>
             <h2>My journey</h2>
-            <div>
-              <div className='flex flex-row justify-between gap-3'>
-                <Image
-                  className="rounded-lg md:self-center"
-                  src="/ucla-logo.png"
-                  alt="UCLA"
-                  width={150}
-                  height={150}
-                  style={{
-                    minWidth: '22%',
-                    height: '100px',
-                    objectFit: "cover"
-                  }}
-                  />
-                <Image
-                  className="rounded-lg md:self-center w-1/2"
-                  src="/ucla-website.jpeg"
-                  alt="UCLA"
-                  width={150}
-                  height={150} 
-                  style={{
-                    minWidth: '50%',
-                    height: '100px',
-                    objectFit: "cover"
-                  }}
-                />
-                <Image
-                  className="rounded-lg md:self-center"
-                  src="/ucla-presentation.jpeg"
-                  alt="UCLA"
-                  width={150}
-                  height={150}
-                  style={{
-                    minWidth: '22%',
-                    height: '100px',
-                    objectFit: "cover"
-                  }}
-                />
-              </div>
-              <h3>Sr. Software Engineer @ UCLA</h3>
-                <p>Working with the University of California at Los Angeles to create a new library access page. Using Vue 2, Rollup, Nuxt, Storybook, Cypress, Git, GraphQL and other modern technologies.
-                I worked developing components based on designs in Figma and going through a rigid development workflow, using e2e tests, peer review and pair programming. The components were initially developed with the intention of being hard coded and then we changed the focus to develop the components as a library and reuse in other applications using rollup and vue 2.
-                I also worked on the UCLA MEAP website. One of the most important pages in the university library.
-                </p>
-            </div>
+            <ExperienceCard
+              role="Sr. Software Engineer"
+              company='UCLA'
+              source="https://ucla.edu"
+              description="Working with the University of California at Los Angeles to create a new library access page. Using Vue 2, Rollup, Nuxt, Storybook, Cypress, Git, GraphQL and other modern technologies.
+              I worked developing components based on designs in Figma and going through a rigid development workflow, using e2e tests, peer review and pair programming. The components were initially developed with the intention of being hard coded and then we changed the focus to develop the components as a library and reuse in other applications using rollup and vue 2.
+              I also worked on the UCLA MEAP website. One of the most important pages in the university library."
+                images={[
+                  {
+                    alt: "UCLA",
+                    image: "ucla-logo.png"
+                  },
+                  {
+                    alt: "UCLA website",
+                    image: "ucla-website.jpeg"
+                  },
+                  {
+                    alt: "UCLA presentation",
+                    image: "ucla-presentation.jpeg"
+                  }
+                ]}
+              startDate="04/2022"
+              endDate='12/2022'
+            />
+            <div className='border-b border-gray-300 dark:border-gray-700 my-7'/>
+            <ExperienceCard
+              role="Mentor"
+              company='Odona'
+              source="https://www.odona.com.br/teacher/matheus-gomes/"
+              description="Front-end technology mentor for diverse niches of people belonging to underrepresented groups. Such as: black people, indigenous people, people who identify with the female gender, people with disabilities, people residing in peripheral regions and LGBTQIA+ people."
+              images={[
+                {
+                  alt: "Odona logo",
+                  image: "odona-logo.jpeg"
+                },
+                {
+                  alt: "Odona website",
+                  image: "odona-website.png"
+                },
+                {
+                  alt: "Odona Mentoree",
+                  image: "odona-mentoree.png"
+                }
+              ]}
+              startDate="04/2022"
+              endDate='now'
+            />
+            <div className='border-b border-gray-300 dark:border-gray-700 my-7'/>
           </section>
-          <h3>Long, 3rd Person</h3>
-          <p>
-            Lee Robinson is the VP of Developer Experience at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web and leads the Next.js community. He leads the
-            community for the React framework Next.js and is an open-source
-            contributor. An educator, writer, and speaker, Lee has created
-            courses on React, Next.js, and web development.
-          </p>
-          <h3>Long, 1st Person</h3>
-          <p>
-            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
-            developers build a faster web. I'm a Next.js contributor and help
-            lead our open-source communities. I'm passionate about frontend
-            development and have created courses on React, Next.js, and web
-            development. I'm an advisor and investor in early stage startups.
-          </p>
-          <h3>Short, 3rd Person</h3>
-          <p>
-            Lee Robinson is the VP of Developer Experience at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web.
-          </p>
-          <h3>Short, 1st Person</h3>
-          <p>
-            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
-            developers build a faster web.
-          </p>
-          <h3>Education</h3>
-          <p>
-            Lee Robinson graduated from Iowa State University with a B.S. in
-            Computer Engineering.
-          </p>
-          <h2>Headshots</h2>
-          <div className="flex space-x-8">
-            <a href="/avatar.jpg">
-              <Image
-                alt="Lee Robinson headshot"
-                width={400}
-                quality={100}
-                src={avatar}
-                className="rounded-md"
-              />
-            </a>
-          </div>
         </div>
       </div>
     </Container>
