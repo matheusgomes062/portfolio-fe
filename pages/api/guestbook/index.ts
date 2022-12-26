ae1dd75fd930630f7f7ea434d43ac41a6c2001c5
Matheus Gomes <matheusgomes062@gmail.com>
2022-12-26 15:38:08 -0300

    fix: add type any to entry

--- pages/api/guestbook/index.ts
+++ pages/api/guestbook/index.ts
@@ -14,7 +14,7 @@ export default async function handler(
     });
 
     return res.json(
-      entries.map((entry) => ({
+      entries.map((entry: any) => ({
         id: entry.id.toString(),
         body: entry.body,
         created_by: entry.created_by,
