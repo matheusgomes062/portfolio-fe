4a9b7deb1c30e5edd6158fac6e55ed2cbd9e29cb
Matheus Gomes <matheusgomes062@gmail.com>
2023-03-09 03:50:10 -0300

    fix: removed sensible data

--- .tina/config.ts
+++ .tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "2d554b8d-a31f-49da-ae86-cf5821da0166", // Get this from tina.io
-  token: "129ce6e1d9e5d092ef2b77125dd3e91aa3981d4d", // Get this from tina.io
+  clientId: process.env.TINACMS_CLIENTID || null, // Get this from tina.io
+  token: process.env.TINACMS_TOKEN|| null, // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
